4e0d61c49dfe8f681cb35d56611970af99d11e26
TheWizard4166 <jamesk281@sbcglobal.net>
2024-11-15 11:49:41 -0600

    Added Toggle and place pawn func

--- main.py
+++ main.py
@@ -10,15 +10,27 @@
 # Assignment:           
 # Date:         15 November 2024
 
+"""
+   Each player will have their own class to assign the variable 
+"""
+
+
 import numpy as np
 import random
 from colorama import Fore, Back, Style
 
 class player:
-    def __init__(self, color):
+    def __init__(self, color, is_turn=False):
         self.color = color
         self.score = 0
-        
+        self.pawns_on_board = 0 
+        self.pawns_held = 4
+        self.is_turn = is_turn
+    
+    def add_pawn_to_board(self):
+        pass
+    def toggle_turn(self):
+        self.is_turn = not self.is_turn
 
 dice = random.randint(1,6)
 print(dice)
