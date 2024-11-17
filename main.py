c72d725fc5e2dc57f1a5048524e2b500fea4df30
jessicareichert15 <jessicareichert@tamu.edu>
2024-11-17 17:23:00 -0600

    Update main.py
    created empty game board

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 
 import numpy as np
 import random
-from colorama import Fore, Back, Style
+import colorama 
 
 class player:
     def __init__(self, color, is_turn=False):
@@ -31,6 +31,34 @@ class player:
         pass
     def toggle_turn(self):
         self.is_turn = not self.is_turn
+
+import colorama
+
+#initialize starting game board
+s = "   "
+y = " "+colorama.Fore.YELLOW +"o"+colorama.Style.RESET_ALL+" "
+b = " "+colorama.Fore.BLUE +"o"+colorama.Style.RESET_ALL+" "
+g = " "+colorama.Fore.GREEN +"o"+colorama.Style.RESET_ALL+" "
+r = " "+colorama.Fore.RED +"o"+colorama.Style.RESET_ALL+" "
+o = " o "
+d = "---"
+u = " | "
+                # 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
+trouble_board = ([s, y, s, y, s, y, s, o, s, b, s, b, s, b, s],
+                 [y, s, s, s, s, s, s, s, s, s, s, s, s, s, b],
+                 [s, s, y, s, s, s, s, s, s, s, s, s, b, s, s],
+                 [y, s, s, y, s, s, s, s, s, s, s, b, s, s, b],
+                 [s, s, s, s, y, s, s, s, s, s, b, s, s, s, s],
+                 [y, s, s, s, s, y, s, s, s, b, s, s, s, s, b],
+                 [s, s, s, s, s, u, d, d, d, u, s, s, s, s, s],
+                 [o, s, s, s, s, u, s, s, s, u, s, s, s, s, o],
+                 [s, s, s, s, s, u, d, d, d, u, s, s, s, s, s],
+                 [g, s, s, s, s, g, s, s, s, r, s, s, s, s, r],
+                 [s, s, s, s, g, s, s, s, s, s, r, s, s, s, s],
+                 [g, s, s, g, s, s, s, s, s, s, s, r, s, s, r],
+                 [s, s, g, s, s, s, s, s ,s ,s, s, s, r, s, s],
+                 [g, s, s, s, s, s, s, s, s, s, s, s, s, s, r],
+                 [s, g, s, g, s, g, s, o, s, r, s, r, s, r, s])
 
 dice = random.randint(1,6)
 print(dice)
