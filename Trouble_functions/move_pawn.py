cc7f0882e8cb20c8feefe26e679c964336a8a429
jessicareichert15 <jessicareichert@tamu.edu>
2024-11-18 11:38:56 -0600

    Update move_pawn.py

--- Trouble_functions/move_pawn.py
+++ Trouble_functions/move_pawn.py
@@ -1,2 +1,4 @@
-def move_player(color, pawn_num, spaces):
+def move_pawn(spaces):
     '''Moves a selected colored pawn the given number of spaces'''
+    pawn_to_move = input("Which pawn on the board would you like to move(1/2/3/4)?: ")
+    print(current_turn[0]+pawn_to_move)
