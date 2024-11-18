651a89ddac2d575c6cd7d1f9df78b2ab480133da
wallacewang76 <wallacewang@tamu.edu>
2024-11-18 11:14:28 -0600

    Update dice_roll.py

--- Trouble_functions/dice_roll.py
+++ Trouble_functions/dice_roll.py
@@ -4,7 +4,7 @@ def dice_roll():
     roll = random.randint(1,6)
     while roll == 6:
         new_pawn = input("Enter a new pawn?(Y/N): ")
-        if new_pawn =+ "Y": 
+        if new_pawn == "Y": 
             add_pawn(current_turn)
         else:
             move_pawn(6)
