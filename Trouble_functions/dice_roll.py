3f2f09d2ed102e5d27d34ff4af428cd67acdbd81
wallacewang76 <wallacewang@tamu.edu>
2024-11-18 11:13:01 -0600

    Update and rename roll_dice.py to dice_roll.py

---
+++ Trouble_functions/dice_roll.py
@@ -0,0 +1,13 @@
+import random
+
+def dice_roll():
+    roll = random.randint(1,6)
+    while roll == 6:
+        new_pawn = input("Enter a new pawn?(Y/N): ")
+        if new_pawn = "Y" 
+            add_pawn(current_turn)
+        else:
+            move_pawn(6)
+        roll = random.randint(1,6)
+  
+    move_pawn(roll)
--- Trouble_functions/roll_dice.py
+++
@@ -1,9 +0,0 @@
-#just one part that needed to work with other functions
-  
-#def roll_dice():
-
-    #if roll == 6:
-        #if pawns_off_board:
-            #add_pawn(color)
-        #else:
-            #move_pawn(6)
