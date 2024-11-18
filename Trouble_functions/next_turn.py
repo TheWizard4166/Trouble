deeb2eb2a67b729b4377e8c54e2468c533f10752
jessicareichert15 <jessicareichert@tamu.edu>
2024-11-18 11:39:21 -0600

    Update next_turn.py

--- Trouble_functions/next_turn.py
+++ Trouble_functions/next_turn.py
@@ -1,6 +1,6 @@
 def next_turn():
     global current_turn
-    turns_dict = {"yellow":1, 1:"yellow", "blue":2, 2:"blue", "green":3, 3:"green", "red":4, 4:"red"}
+    turns_dict = {"yellow":1, 1:"yellow", "blue":2, 2:"blue", "red":3, 3:"red", "green":4, 4:"green"}
     if turns_dict[current_turn]+1 >=5:
         current_turn = (turns_dict[turns_dict[current_turn]-3])
     else:
